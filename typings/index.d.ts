ebe9f6697688e31fa967ebe153edad3bb7552629
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-01 14:16:12 -0400

    added more exported types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,6 +2,24 @@ import record from './record';
 import { Replayer } from './replay';
 import { mirror } from './utils';
 import * as utils from './utils';
-export { EventType, IncrementalSource, MouseInteractions, ReplayerEvents, } from './types';
+export {
+  EventType,
+  IncrementalSource,
+  MouseInteractions,
+  ReplayerEvents,
+  // added by @jay-khatri for convenience.
+  eventWithTime,
+  incrementalData,
+  mutationData,
+  mousemoveData,
+  mouseInteractionData,
+  scrollData,
+  viewportResizeData,
+  inputData,
+  mediaInteractionData,
+  styleSheetRuleData,
+  canvasMutationData,
+  fontData,
+} from './types';
 declare const addCustomEvent: <T>(tag: string, payload: T) => void;
 export { record, addCustomEvent, Replayer, mirror, utils };
